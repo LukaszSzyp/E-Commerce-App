7363e25dd5bc31b4fc015d3213d96d6fac741ad7
Lukasz Szypulewski <szyp86@gmail.com>
2022-01-10 22:55:00 +0100

    add 3 element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SerchContainer = styled.div``;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SerchContainer>input icon</SerchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
