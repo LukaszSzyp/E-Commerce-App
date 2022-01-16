422389267923a8764b5cb5ee4518cd4ff3a31a76
Lukasz Szypulewski <szyp86@gmail.com>
2022-01-16 14:25:26 +0100

    add footer component and styled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Products from "../components/Products";
 import Navbar from "./../components/Navbar";
 import Slider from "./../components/Slider";
 import Newsletter from "./../components/Newsletter";
+import Footer from "./../components/Footer";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Categories />
       <Products />
       <Newsletter />
+      <Footer />
     </div>
   );
 };
