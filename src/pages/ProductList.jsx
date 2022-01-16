9b7ff585f81a746d797c50594e494a15d8dd8b80
Lukasz Szypulewski <szyp86@gmail.com>
2022-01-16 14:56:06 +0100

    add new page ProductList

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Navbar from "./../components/Navbar";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>filter1</Filter>
+        <Filter>filter2</Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
